a06f6435981258cc91c21ddc480c701d1dc7b423
Che <43485962+c-git@users.noreply.github.com>
2023-03-06 13:36:04 -0500

    diagonal-difference

---
+++ src/bin/diagonal-difference.rs
@@ -0,0 +1,49 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+#[allow(non_snake_case)]
+fn diagonalDifference(arr: &[Vec<i32>]) -> i32 {
+    todo!()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
+
+    let mut arr: Vec<Vec<i32>> = Vec::with_capacity(n as usize);
+
+    for i in 0..n as usize {
+        arr.push(Vec::with_capacity(n as usize));
+
+        arr[i] = stdin_iterator
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_end()
+            .split(' ')
+            .map(|s| s.to_string().parse::<i32>().unwrap())
+            .collect();
+    }
+
+    let result = diagonalDifference(&arr);
+
+    writeln!(&mut fptr, "{result}").ok();
+}
