af03a3ed060188561ebcd76de293cb3f84687153
Che <43485962+c-git@users.noreply.github.com>
2023-03-06 13:28:39 -0500

    flipping-bits

---
+++ src/bin/flipping-bits.rs
@@ -0,0 +1,43 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+#[allow(non_snake_case)]
+fn flippingBits(n: i64) -> i64 {
+    (!(n as u32)) as i64
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let q = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
+
+    for _ in 0..q {
+        let n = stdin_iterator
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim()
+            .parse::<i64>()
+            .unwrap();
+
+        let result = flippingBits(n);
+
+        writeln!(&mut fptr, "{result}").ok();
+    }
+}
--- tests/run_samples.rs
+++ tests/run_samples.rs
@@ -10,7 +10,7 @@ fn get_problem_settings() -> ProblemSettings {
     // Change problem name here <----------------------->
 
     ProblemSettings {
-        problem_name: "grading".to_string(),
+        problem_name: "flipping-bits".to_string(),
         // eval_type: EvalType::Stdout,
         eval_type: EvalType::File(Default::default()),
     }
