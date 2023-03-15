11462d827ea76d4b540d44d20b08b4af95948d8d
Che <43485962+c-git@users.noreply.github.com>
2023-03-15 16:40:14 -0400

    strings-xor
    Submission 1, rejected but answers seems correct

---
+++ src/bin/strings-xor.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn strings_xor(s: &str, t: &str) -> String {
+    let mut res = "".to_string();
+    for (s_i, t_i) in s.chars().zip(t.chars()) {
+        if s_i != t_i && (s_i == '1' || t_i == '0') {
+            res += "1";
+        } else {
+            res += "0";
+        }
+    }
+    res
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let s = stdin_iterator.next().unwrap().unwrap().trim().to_string();
+    let t = stdin_iterator.next().unwrap().unwrap().trim().to_string();
+
+    let result = strings_xor(&s, &t);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
--- tests/run_samples.rs
+++ tests/run_samples.rs
@@ -10,7 +10,7 @@ fn get_problem_settings() -> ProblemSettings {
     // Change problem name here <----------------------->
 
     ProblemSettings {
-        problem_name: "the-birthday-bar".to_string(),
+        problem_name: "strings-xor".to_string(),
         // eval_type: EvalType::Stdout,
         eval_type: EvalType::File(Default::default()),
     }
